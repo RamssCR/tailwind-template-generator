8c88067fd5b7fe1b364f99bad5c0259155693c79
RamssCR <ramsesgpm@gmail.com>
2025-08-20 14:31:56 -0500

    build: setup vitest.config file

---
+++ vitest.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    setupFiles: ['./test/setupTest.js'],
+    include: ['test/**/*.spec.js'],
+    threads: false,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'lcov'],
+      all: true,
+      thresholds: {
+        lines: 90,
+        functions: 90,
+        branches: 90,
+        statements: 90
+      },
+      exclude: [
+        'test/**',
+        'lib/index.js',
+        'bin/index.js',
+        '**/*.config.js',
+        'scripts/**',
+        'coverage/**',
+        'html/**'
+      ]
+    }
+  }
+})
